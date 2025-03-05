342e9f334428f7c2a94a53899c9dd6f2e5bee0a4
Mara Federmair <mara@klingt.org>
2025-03-05 18:37:50 +0100

    init

---
+++ src/main/java/at/fhbfi/tourism/noetourismapp/NoeTourismAppApplication.java
@@ -0,0 +1,13 @@
+package at.fhbfi.tourism.noetourismapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoeTourismAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoeTourismAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhbfi/tourism/noetourismapp/NoeTourismAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhbfi.tourism.noetourismapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoeTourismAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
